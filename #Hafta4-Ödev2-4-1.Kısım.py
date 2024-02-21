1bee3ebce0068e19761994796e894f38e02ef3b1
Emin Kaya <126521498+ligernus@users.noreply.github.com>
2024-02-22 02:09:12 +0300

    Update #Hafta4-Ödev2-4-1.Kısım.py

--- #Hafta4-Ödev2-4-1.Kısım.py
+++ #Hafta4-Ödev2-4-1.Kısım.py
@@ -1,9 +1,8 @@
-#Hafta4-Ödev2-4.Kısım-2
+#Hafta4-Ödev2-4.Kısım-1
 def kullaniciadi_belirle():  
     kk=str(input("Kullanıcı Adını Giriniz: "))
     print("Kullanıcı Adınızı Oluşturdunuz ",kk)
-    return kk
-   
+kullaniciadi_belirle()    
 def sifre_belirle():
     while True:
      kk_sifre=input("Sifrenizi Belirleyiniz: ")
@@ -12,21 +11,14 @@ def sifre_belirle():
         return kk_sifre
      else:
         print("şifrenizi tekrardan oluşturun ")
-
+sifre_belirle()
 def giris():
-     girilen_kk=kullaniciadi_belirle()
-     girilen_sifre=sifre_belirle()
-     giris_hakki=3
-     while giris_hakki>0:
-        dogru_kk=input("Kullanici Adini Doğrulamak Adına Tekrar Giriniz: ")
-        dogru_sifre=input("Doğru Şifrenizi Doğrulamak Amacıyla Tekrar Giriniz: ")
+        girilen_kk=kullaniciadi_belirle()
+        girilen_sifre=sifre_belirle()
+        dogru_kk=girilen_kk
+        dogru_sifre=girilen_sifre
         if girilen_kk ==dogru_kk and girilen_sifre ==dogru_sifre:
                 print("Giriş Başarılı")
-                break
         else:
-                giris_hakki -=1
-                if giris_hakki==0:
-                        print("Üç Kez yalnız girdiniz belirli süre sonra tekrar deneyiniz.")
-                else:
-                        print("Kullanıcı Adınızı veya Şifrenizi yanlış girdiniz.",giris_hakki)
+                print("Kullanıcı Adınızı veya Şifrenizi yanlış girdiniz.")
 giris()                        
